63cdaedc0004359ad1c2bcbc567e3f4c651585e6
Diego Rocha <diego.webdg@gmail.com>
2014-05-12 23:19:03 -0300

    Produtos

--- Zum/header.php
+++ Zum/header.php
@@ -3,7 +3,6 @@
 
 <head>
     <meta http-equiv="Content-Type" content="text/html;charset=utf-8" >
-    <meta name="viewport" content="width=device-width, user-scalable=no">
     <meta name="description" content="">
     
     <?php $output = '<title>%TITLE%</title>'; ?>
---
+++ Zum/produtos.php
@@ -0,0 +1,150 @@
+<?php 
+    include('header.php');
+    $title = "Produtos - Zum";
+    $output = str_replace('%TITLE%', $title, $output);
+    echo $output;
+?>
+
+<link rel="stylesheet" href="css/produtos.css">
+
+<!--Content-->
+<div id="content">
+    <div class="content-center">
+        <!--Titles-->
+        <div class="titles">
+            <h1>produtos</h1>
+            <img src="images/produtos/produtos.png" alt="Ilustração de uma refeição" title="Veja nossos produtos!">
+            <div class="clear"></div>
+        </div>
+        <!--End of Titles-->
+        
+        <!--Submenu-->
+        <div class="submenu">
+            <a href="#" title="todos" class="active">todos <span>65</span></a>
+            <a href="#" title="naturais">naturais <span>3</span></a>
+            <a href="#" title="sucos">sucos <span>4</span></a>
+            <a href="#" title="lanches">lanches <span>2</span></a>
+            <a href="#" title="bolos">bolos <span>5</span></a>
+            <a href="#" title="pastas">pastas <span>4</span></a>
+            <a href="#" title="molhos">molhos <span>5</span></a>
+            <a href="#" title="sorvetes">sorvetes <span>7</span></a>
+            <a href="#" title="cafés">cafés <span>4</span></a>
+            <a href="#" title="vitaminas">vitaminas <span>3</span></a>
+            <a href="#" title="verdes">verdes <span>1</span></a>
+        </div>
+        <!--End of Submenu-->
+        
+        <!--Produtos-->
+        <div class="produtos">
+            <div class="item big">
+                <a href="#" title="">
+                    <img src="images/produtos/item-big.jpg" width="650" height="434" alt="" title="">
+                    <div class="description">
+                        <h2>Sanduiche Natural</h2>
+                        <h3>Ingredientes</h3>
+                        <p>Pão integral, presunto, queijo, alface molho especial e tomate.</p>
+                    </div>
+                </a>
+            </div>
+            
+            <div class="item">
+                <a href="#" title="">
+                    <img src="images/produtos/item.jpg" width="300" height="150" alt="" title="">
+                    <div class="description">
+                        <h2>Sanduiche Natural</h2>
+                        <h3>Ingredientes</h3>
+                        <p>Pão integral, presunto, queijo, alface molho especial e tomate.</p>
+                    </div>
+                </a>
+            </div>
+            
+            <div class="item">
+                <a href="#" title="">
+                    <img src="images/produtos/item.jpg" width="300" height="150" alt="" title="">
+                    <div class="description">
+                        <h2>Sanduiche Natural</h2>
+                        <h3>Ingredientes</h3>
+                        <p>Pão integral, presunto, queijo, alface molho especial e tomate.</p>
+                    </div>
+                </a>
+            </div>
+            
+            <div class="item">
+                <a href="#" title="">
+                    <img src="images/produtos/item.jpg" width="300" height="150" alt="" title="">
+                    <div class="description">
+                        <h2>Sanduiche Natural</h2>
+                        <h3>Ingredientes</h3>
+                        <p>Pão integral, presunto, queijo, alface molho especial e tomate.</p>
+                    </div>
+                </a>
+            </div>
+            
+            <div class="item">
+                <a href="#" title="">
+                    <img src="images/produtos/item.jpg" width="300" height="150" alt="" title="">
+                    <div class="description">
+                        <h2>Sanduiche Natural</h2>
+                        <h3>Ingredientes</h3>
+                        <p>Pão integral, presunto, queijo, alface molho especial e tomate.</p>
+                    </div>
+                </a>
+            </div>
+            <div class="item">
+                <a href="#" title="">
+                    <img src="images/produtos/item.jpg" width="300" height="150" alt="" title="">
+                    <div class="description">
+                        <h2>Sanduiche Natural</h2>
+                        <h3>Ingredientes</h3>
+                        <p>Pão integral, presunto, queijo, alface molho especial e tomate.</p>
+                    </div>
+                </a>
+            </div>
+            
+            <div class="item">
+                <a href="#" title="">
+                    <img src="images/produtos/item.jpg" width="300" height="150" alt="" title="">
+                    <div class="description">
+                        <h2>Sanduiche Natural</h2>
+                        <h3>Ingredientes</h3>
+                        <p>Pão integral, presunto, queijo, alface molho especial e tomate.</p>
+                    </div>
+                </a>
+            </div>
+            
+            <div class="item big">
+                <a href="#" title="">
+                    <img src="images/produtos/item-big.jpg" width="650" height="434" alt="" title="">
+                    <div class="description">
+                        <h2>Sanduiche Natural</h2>
+                        <h3>Ingredientes</h3>
+                        <p>Pão integral, presunto, queijo, alface molho especial e tomate.</p>
+                    </div>
+                </a>
+            </div>
+            <div class="clear"></div>
+        </div>
+        <!--End of Produtos-->
+        
+    </div>
+</div>
+<!--End of Content-->
+        
+<?php include('footer.php'); ?>
+
+<script type="text/javascript" src="js/masonry.pkgd.min.js"></script>
+<script type="text/javascript">
+    $(document).ready(function(e){
+        //Submenu
+        $('.submenu a').click(function(e){
+            $('.submenu a').removeClass('active');
+            $(this).addClass('active');
+        });
+        
+        //Columns
+        $('#content .produtos').masonry({
+          columnWidth: 1,
+          itemSelector: '.item'
+        });
+    });
+</script>
