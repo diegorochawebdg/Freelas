c7e26d5ab70ba946b8240424225f783bcdb3762f
Diego Rocha <diego.webdg@gmail.com>
2014-06-02 23:08:12 -0300

    Magile - alterações

--- magile/footer.php
+++ magile/footer.php
@@ -5,12 +5,6 @@
                                 <div class="container">
                                     <address><span>Endereço:</span> Rua Cachoeira do Sul, 378 - São Paulo - SP</address>
                                     <span class="tel">Telefone: <a href="tel:01136217922">11 3621-7922</a></span>
-                                    <div class="logos">
-                                        <img src="<?php bloginfo('template_directory'); ?>/images/magile-transportes-logo-footer.png" alt="Magile transportes" title="Magile transportes" class="magile-footer">
-                                        <img src="<?php bloginfo('template_directory'); ?>/images/grupo-gergo-logo-footer.png" alt="Grupo Gergo" title="Grupo Gergo">
-                                        <img src="<?php bloginfo('template_directory'); ?>/images/iso9001-footer.png" alt="ISO 9001" title="ISO 9001">
-                                        <div class="clear"></div>
-                                    </div>
                                     <div class="clear"></div>
                                 </div> <!-- end .container -->
                             </div> <!-- end #footer-center -->
