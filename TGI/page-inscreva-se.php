b0a3ea0bdec89936e84752cd52687074d79e8784
Diego Rocha <diego.webdg@gmail.com>
2014-10-21 17:57:52 -0200

    TGI
    Inscreva-se

--- TGI/page-inscreva-se.php
+++ TGI/page-inscreva-se.php
@@ -8,18 +8,59 @@
 			<div class="row-fluid">
 				<h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit molestias, dolores minus ad libero. Dolores sit repudiandae rem ab, quam minus cum illum voluptatum accusamus iure inventore odio eligendi, necessitatibus.</h1>
 				<form action="">
-					<input type="text" name="" id="" placeholder="">
-					<input type="text" name="" id="" placeholder="">
-					<input type="text" name="" id="" placeholder="">
-					<div class="select">
-						<select name="" id="">
-							<option value=""></option>
-						</select></div>
+					<input type="text" name="nome" id="nome" placeholder="Nome Completo">
+					<input type="email" name="email" id="email" placeholder="E-mail">
+					<input type="password" name="senha" id="senha" placeholder="Senha">
 					<div class="select">
 						<select name="estado" id="estado">
-							<option value=""></option>
+							<option value="estado">Estado</option> 
+                            <option value="ac">Acre</option> 
+                            <option value="al">Alagoas</option> 
+                            <option value="am">Amazonas</option> 
+                            <option value="ap">Amapá</option> 
+                            <option value="ba">Bahia</option> 
+                            <option value="ce">Ceará</option> 
+                            <option value="df">Distrito Federal</option> 
+                            <option value="es">Espírito Santo</option> 
+                            <option value="go">Goiás</option> 
+                            <option value="ma">Maranhão</option> 
+                            <option value="mt">Mato Grosso</option> 
+                            <option value="ms">Mato Grosso do Sul</option> 
+                            <option value="mg">Minas Gerais</option> 
+                            <option value="pa">Pará</option> 
+                            <option value="pb">Paraíba</option> 
+                            <option value="pr">Paraná</option> 
+                            <option value="pe">Pernambuco</option> 
+                            <option value="pi">Piauí</option> 
+                            <option value="rj">Rio de Janeiro</option> 
+                            <option value="rn">Rio Grande do Norte</option> 
+                            <option value="ro">Rondônia</option> 
+                            <option value="rs">Rio Grande do Sul</option> 
+                            <option value="rr">Roraima</option> 
+                            <option value="sc">Santa Catarina</option> 
+                            <option value="se">Sergipe</option> 
+                            <option value="sp">São Paulo</option> 
+                            <option value="to">Tocantins</option> 
 						</select>
-					</div>
+					<input type="text" name="cidade" id="cidade" placeholder="Cidade">
+					<div class="clearfix"></div>
+					<span>Data de nascimento</span>
+					<input type="text" name="dia" id="dia" placeholder="Dia">
+					<input type="text" name="mes" id="mes" placeholder="Mês">
+					<input type="text" name="ano" id="ano" placeholder="Ano">
+					<div class="clearfix"></div>
+
+					<label>Sexo:</label>
+					<input type="radio" name="sexo" id="masculino">
+					<label for="masculino">Masculino</label>
+					<input type="radio" name="sexo" id="feminino">
+					<label for="feminino">Feminino</label>
+					<div class="clearfix"></div>
+
+					<input type="checkbox" name="termos" id="termos" checked>
+					<label for="termos">Ao clicar em Participar, você concorda com nossos <a href="#" target="_blank">Termos e Política de Privacidade</a></label>
+
+					<button type="submit" name="participar" id="participar">Participar</button>
 				</form>
 			</div>
 			
