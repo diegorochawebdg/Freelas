06629f8c58737b01d0785615028ce4583595bf72
Diego Rocha <diego.webdg@gmail.com>
2014-12-06 14:05:37 -0200

    WGY
    @init();

---
+++ WGY Indulge/footer.php
@@ -0,0 +1,34 @@
+		<!-- Footer -->
+		<footer>
+			<div class="container">
+				<!-- Social -->
+				<div class="footer-social">
+					<ul>
+						<li><a href="" title="Facebook">Facebook</a></li>
+						<li><a href="" title="Twitter">Twitter</a></li>
+						<li><a href="" title="Instagram">Instagram</a></li>
+					</ul>
+				</div>
+				<!-- End Social -->
+
+				<!-- Footer Links -->
+				<div class="footerlinks pull-left">
+					<ul>
+						<li><a href="" title="Home">Home</a></li>
+						<li><a href="" title="About Us">About Us</a></li>
+						<li><a href="" title="Our Services">Our Services</a></li>
+						<li><a href="" title="Insider">Insider</a></li>
+					</ul>
+					<ul>
+						<li><a href="" title="Privacy">Privacy</a></li>
+						<li><a href="" title="Terms &amp; Conditions">Terms &amp; Conditions</a></li>
+						<li><a href="" title="Contact">Contact</a></li>
+						<li><a href="" title="Contact"></a></li>
+					</ul>
+				</div>
+				<!-- End Footer Links -->
+			</div>
+		</footer>
+		<!-- End Footer -->
+    </body>
+</html>
---
+++ WGY Indulge/header.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en-us">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="Diego Rocha">
+        <link rel="icon" href="favicon.ico">
+
+        <title>Título de exemplo</title>
+		
+		<!-- Styles -->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/bootstrap.css.map">
+        <link rel="stylesheet" href="css/main.css">
+        <!-- End Styles -->
+
+        <!-- Scripts -->
+
+        <script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
+        <script type="text/javascript" src="js/html5shiv.js"></script>
+        <!-- End Scripts -->
+    </head>
+
+    <body>
+    	
+    	<!-- Header -->
+		<header>
+			<div class="container">
+				<nav class="navbar navbar-default" role="navigation">
+					<div class="container-fluid">
+
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+								<span class="sr-only">Menu</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+							<a class="navbar-brand" href="/" title="WGY Indulge"><img src="" alt="WGY Indulge" title="WGY Indulge"></a>
+						</div>
+
+						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+							<ul class="nav navbar-nav">
+								<li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+								<li><a href="#">Link</a></li>
+							</ul>
+							<ul class="nav navbar-nav navbar-right">
+								<li><a href="#">Link</a></li>
+							</ul>
+						</div>
+					</div>
+				</nav>
+			</div>
+		</header>
+		<!-- End Header -->
+		
+		<!-- Content -->
+		<section id="content">
+			
+		</section>
+		<!-- End Content -->
+
+    </body>
+</html>
