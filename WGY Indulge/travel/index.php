871eb9b4dc028e8d1f43e58068e4b81a4d6b1021
Diego Rocha <diego.webdg@gmail.com>
2014-12-07 16:36:42 -0200

    Travel
    init();

---
+++ WGY Indulge/travel/index.php
@@ -0,0 +1,168 @@
+<?php include('../header.php'); ?>
+<link rel="stylesheet" href="/css/travel.css">
+
+<!-- Main Content -->
+<section id="main-content" class="section-padding">
+	<div class="container">
+		<h1>Travel</h1>
+		<div class="main-txt">
+			<p>WGY TRAVEL is a full service in-house travel planner with worldwide access. Based on the client’s needs, we offer a variety of services to make life that much easier during personal or business travel anywhere in the world.</p>
+		</div>
+	</div>
+</section>
+<!-- End Main Content -->
+
+<!-- Travel Blocks -->
+<div id="travel-blocks" class="section-padding">
+	<div class="container">
+		<ul>
+			<li class="col-md-4">
+				<div class="relative">
+					<div class="blocks-text">
+						<div class="text-relative">
+							<span>worldwide access</span>
+						</div>
+					</div>
+				</div>
+			</li>
+			<li class="col-md-4">
+				<div class="relative">
+					<div class="blocks-text">
+						<div class="text-relative">
+							<span>Luxurious Accommodations</span>
+						</div>
+					</div>
+				</div>
+			</li>
+			<li class="col-md-4">
+				<div class="relative">
+					<div class="blocks-text">
+						<div class="text-relative">
+							<span>Certified by the IATAN worldwide travel network</span>
+						</div>
+					</div>
+				</div>
+			</li>
+			<li class="col-md-4">
+				<div class="relative">
+					<div class="blocks-text">
+						<div class="text-relative">
+							<span>Personal and Corporate Planning</span>
+						</div>
+					</div>
+				</div>
+			</li>
+			<li class="col-md-4">
+				<div class="relative">
+					<div class="blocks-text">
+						<div class="text-relative">
+							<span>Best Rates in Business &amp; First Class Tickets</span>
+						</div>
+					</div>
+				</div>
+			</li>
+			<li class="col-md-4">
+				<div class="relative">
+					<div class="blocks-text">
+						<div class="text-relative">
+							<span>Complimentary upgrades based on availability</span>
+						</div>
+					</div>
+				</div>
+			</li>
+		</ul>
+	</div>
+</div>
+<!-- End Travel Blocks -->
+
+<!-- Themes Slider -->
+<div id="themes-slider">
+	<div class="container">
+		<h2>Themes</h2>
+		<div class="row">
+			<div id="themes-slider">
+				<div class="slide">
+					<ul>
+						<li class="col-md-3">
+							<div class="relative">
+								<div class="blocks-text">
+									<div class="text-relative">
+										<span>Road trip</span>
+									</div>
+								</div>
+							</div>
+						</li>
+						<li class="col-md-3">
+							<div class="relative">
+								<div class="blocks-text">
+									<div class="text-relative">
+										<span>Montain</span>
+									</div>
+								</div>
+							</div>
+						</li>
+						<li class="col-md-3">
+							<div class="relative">
+								<div class="blocks-text">
+									<div class="text-relative">
+										<span>Island</span>
+									</div>
+								</div>
+							</div>
+						</li>
+						<li class="col-md-3">
+							<div class="relative">
+								<div class="blocks-text">
+									<div class="text-relative">
+										<span>Adventure</span>
+									</div>
+								</div>
+							</div>
+						</li>
+						<li class="col-md-3">
+							<div class="relative">
+								<div class="blocks-text">
+									<div class="text-relative">
+										<span>Nightlife</span>
+									</div>
+								</div>
+							</div>
+						</li>
+						<li class="col-md-3">
+							<div class="relative">
+								<div class="blocks-text">
+									<div class="text-relative">
+										<span>Love</span>
+									</div>
+								</div>
+							</div>
+						</li>
+						<li class="col-md-3">
+							<div class="relative">
+								<div class="blocks-text">
+									<div class="text-relative">
+										<span>Gourmet</span>
+									</div>
+								</div>
+							</div>
+						</li>
+						<li class="col-md-3">
+							<div class="relative">
+								<div class="blocks-text">
+									<div class="text-relative">
+										<span>Capitals</span>
+									</div>
+								</div>
+							</div>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+
+		
+	</div>
+</div>
+<!-- End Themes Slider -->
+
+<?php include('../footer.php'); ?>
