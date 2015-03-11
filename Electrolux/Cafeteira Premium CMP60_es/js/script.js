61211befc4138cd1a04ad3b7aa40c8816e1ab24b
Diego Rocha <diego.webdg@gmail.com>
2015-03-10 21:55:46 -0300

    [Electrolux] Cafeteira CMP60 (espanhol)
    Tradução da página da cafeteira;
    Compressão das fontes;

---
+++ Electrolux/Cafeteira Premium CMP60_es/js/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+	$('a').click(function(event) {
+        if($(this).attr('href', '#')){
+            event.preventDefault();
+        }
+    });
+
+    //Hotspots
+    $('.hotspots li.item1').hover(function() {
+    	$(this).parent().parent().parent().children('.hotspots-content').children('.item1').stop().fadeIn('fast');
+    }, function() {
+    	$(this).parent().parent().parent().children('.hotspots-content').children('.item1').stop().fadeOut('fast');
+    });
+
+    $('.hotspots li.item2').hover(function() {
+    	$(this).parent().parent().parent().children('.hotspots-content').children('.item2').stop().fadeIn('fast');
+    }, function() {
+    	$(this).parent().parent().parent().children('.hotspots-content').children('.item2').stop().fadeOut('fast');
+    });
+
+    //Animate
+    var intervalo = window.setInterval(lerolero, 1000);
+    function lerolero() {
+        $('#features .animate img').toggleClass('active');
+    }
+});
