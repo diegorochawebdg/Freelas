56338172dbba4d47a1dbab35c93ad000bc2407c1
Diego Rocha <diego.webdg@gmail.com>
2015-03-23 02:29:16 -0300

    [Epson] L455
    Animated;

--- Epson/Epson L455/js/script.js
+++ Epson/Epson L455/js/script.js
@@ -19,8 +19,51 @@ $(document).ready(function() {
     });
 
     //Animate
-    var intervalo = window.setInterval(lerolero, 1000);
-    function lerolero() {
-        $('#features .animate img').toggleClass('active');
+    var intervalo = window.setInterval(abastecimento, 2000);
+    function abastecimento() {
+        $('header .mao-animacao').toggleClass('active');
     }
+
+    //Appear
+    $('.animated').appear();
+
+    //Fade in Down
+    $('header hgroup, header .ecotank, p.vendedor-txt, section#versatil-e-completa .txt, .conexao-wifi, .wifi, .wifi-direct img, .epson-connect-txt div, .email-print, .cloud-print').on('appear', function(event, $all_appeared_elements) {
+        $(this).addClass('fadeInDown');
+    });
+    $('header hgroup, header .ecotank, p.vendedor-txt, section#versatil-e-completa .txt, .conexao-wifi, .wifi, .wifi-direct img, .epson-connect-txt div, .email-print, .cloud-print').on('disappear', function(event, $all_appeared_elements) {
+        $(this).removeClass('fadeInDown');
+    });
+
+    /*Fade In Right*/
+    $('.recarregue').on('appear', function(event, $all_appeared_elements) {
+        $(this).addClass('fadeInRight');
+    });
+    $('.recarregue').on('disappear', function(event, $all_appeared_elements) {
+        $(this).removeClass('fadeInRight');
+    });
+
+    /*Fade In Right Big*/
+    $('.economica, .l455, .multifuncional-convencional').on('appear', function(event, $all_appeared_elements) {
+        $(this).addClass('fadeInRight');
+    });
+    $('.economica, .l455, .multifuncional-convencional').on('disappear', function(event, $all_appeared_elements) {
+        $(this).removeClass('fadeInRight');
+    });
+
+    /*Fade In Up*/
+    $('section#versatil-e-completa strong').on('appear', function(event, $all_appeared_elements) {
+        $(this).addClass('fadeInUp');
+    });
+    $('section#versatil-e-completa strong').on('disappear', function(event, $all_appeared_elements) {
+        $(this).removeClass('fadeInUp');
+    });
+
+    //RollIn
+    $('.impressoes-tinta .animacao1, .impressoes-tinta .animacao2, .impressoes-tinta .animacao3').on('appear', function(event, $all_appeared_elements) {
+        $(this).addClass('rollIn');
+    });
+    $('.impressoes-tinta .animacao1, .impressoes-tinta .animacao2, .impressoes-tinta .animacao3').on('disappear', function(event, $all_appeared_elements) {
+        $(this).removeClass('rollIn');
+    });
 });
