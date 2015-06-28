75bc5e50cb0a69509dacb3cabac36459781b3c55
Diego Rocha <diego.webdg@gmail.com>
2015-06-28 18:58:55 -0300

    Ajustes
    Ajustes no repositório e adição dos projetores

---
+++ Epson/Projetor X21/js/script.js
@@ -0,0 +1,22 @@
+jQuery(document).ready(function() {
+	jQuery('a').click(function(e) {
+        if(jQuery(this).attr('href', '#')){
+            e.preventDefault();
+        }
+    });
+
+    //Hotspots
+    jQuery('.hotspots li.item1').hover(function() {
+    	jQuery(this).parent().parent().parent().children('.hotspots-content').children('.item1').addClass('active');
+    }, function() {
+    	jQuery(this).parent().parent().parent().children('.hotspots-content').children('.item1').removeClass('active');
+    });
+
+    //Animate
+    $(window).load(function() {
+        jQuery('header .parallax-layer').parallax({
+            mouseport: jQuery('.parallax-area'),
+            xparallax: false
+        });
+    });
+});
